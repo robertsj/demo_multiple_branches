c3a105eec89f7b99f929ed977e3d061f1314967b
Jeremy Roberts <jaroberts@ksu.edu>
2018-09-07 10:30:20 -0500

    Added some more variables with comments.

--- some_basics.py
+++ some_basics.py
@@ -1,5 +1,12 @@
+# Some variables of simple types.
 a = 123
 b = 1.23
 c = "123"
 d = '123'
+e = True
+
+# Some variables of Python container types.
+f = [a, b, c] 
+g = (a, b, c)
+h = {'a': a, b: 'b', d: e}
 
