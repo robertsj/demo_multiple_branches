852e6a5f174cd269d0d6c85ab566e4e512d7fa6d
Jeremy Roberts <jaroberts@ksu.edu>
2018-09-07 10:31:44 -0500

    Modified comments for IPython cells.  Demonstrated a print statement.

--- some_basics.py
+++ some_basics.py
@@ -1,3 +1,4 @@
+#%%
 # Some variables of simple types.
 a = 123
 b = 1.23
@@ -5,8 +6,12 @@ c = "123"
 d = '123'
 e = True
 
+#%%
 # Some variables of Python container types.
 f = [a, b, c] 
 g = (a, b, c)
 h = {'a': a, b: 'b', d: e}
 
+#%%
+# Printing contents
+print("f = ", f)
